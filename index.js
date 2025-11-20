bb613a730d01fa2af36376bef482e2d342822ce4
Azeem Raisdeen <139336834+azeemraisdeen@users.noreply.github.com>
2025-11-21 01:19:38 +0530

    Added new code and resources for the landing page

---
+++ index.js
@@ -0,0 +1,16 @@
+ const mobileBtn = document.querySelector('.mobile-menu-btn');
+const navWrapper = document.querySelector('.nav-wrapper');
+const navLinks = document.querySelectorAll('.nav-link');
+
+mobileBtn.addEventListener('click', () => {
+    mobileBtn.classList.toggle('active');
+    navWrapper.classList.toggle('active');
+});
+
+// Close menu when clicking a link
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        mobileBtn.classList.remove('active');
+        navWrapper.classList.remove('active');
+    });
+});
